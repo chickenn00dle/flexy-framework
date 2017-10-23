7db63c6388f74f826074da285ebd0a41cba154f6
Rasmy Nguyen <rtnguyen87@gmail.com>
2017-10-23 22:21:27 +0900

    Adds hamburger icon to header for mobile.

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,3 +1,13 @@
-var $;
+let $;
 
 $ = require('jquery');
+
+console.log('scripts ready');
+
+$('.hamburger-outer').click(function() {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).addClass('active');
+    }
+});
