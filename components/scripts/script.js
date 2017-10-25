85925024e1833a12079ca623e397837356d25a0b
Rasmy Nguyen <rtnguyen87@gmail.com>
2017-10-25 22:25:40 +0900

    Adds megamenu dropdown to header.

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -7,7 +7,9 @@ console.log('scripts ready');
 $('.hamburger-outer').click(function() {
     if ($(this).hasClass('active')) {
         $(this).removeClass('active');
+        $('.nav-mobile').addClass('hidden').removeClass('slide-in');
     } else {
         $(this).addClass('active');
+        $('.nav-mobile').removeClass('hidden').addClass('slide-in');
     }
 });
