6306161871aa4fd592a6f26d0d44a96333d394a2
Rasmy Nguyen <rtnguyen87@gmail.com>
2017-10-26 12:34:44 +0900

    [Bugfix] Fix scrolling issue when megamenu is active.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -6,9 +6,11 @@ $ = require('jquery');
 $('.hamburger-outer').click(function() {
     if ($(this).hasClass('active')) {
         $(this).removeClass('active');
+        $('body').removeClass('overflow-h');
         $('.nav-links').addClass('slide-out').removeClass('slide-in').delay(3000).addClass('hidden-mobile').removeClass('slide-out');
     } else {
         $(this).addClass('active');
+        $('body').addClass('overflow-h');
         $('.nav-links').removeClass('hidden-mobile').addClass('slide-in');
     }
 });
