979f702bffebe5596a2705acab75949b92def1f1
Rasmy Nguyen <rtnguyen87@gmail.com>
2017-10-26 13:28:36 +0900

    [Bugfix] Lock scroll for iphone when megamenu is open.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -6,11 +6,11 @@ $ = require('jquery');
 $('.hamburger-outer').click(function() {
     if ($(this).hasClass('active')) {
         $(this).removeClass('active');
-        $('body').removeClass('overflow-h');
+        $('body').removeClass('lock');
         $('.nav-links').addClass('slide-out').removeClass('slide-in').delay(3000).addClass('hidden-mobile').removeClass('slide-out');
     } else {
         $(this).addClass('active');
-        $('body').addClass('overflow-h');
+        $('body').addClass('lock');
         $('.nav-links').removeClass('hidden-mobile').addClass('slide-in');
     }
 });
