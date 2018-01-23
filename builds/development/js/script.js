fbd09f2a1e47a2c2d320e6fe5b0f66e8ac8c070a
Rasmy Nguyen <rtnguyen87@gmail.com>
2018-01-23 13:10:13 +0900

    Modify header and clean up related js.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,31 +2,26 @@
 window.onload = function() {
     console.log('Javascript Loaded');
 
-    let htmlClasses = document.querySelector("html").classList,
-        bodyClasses = document.querySelector("body").classList,
-        navLinksClasses = document.querySelector(".nav-links").classList,
-        hbOuter = document.querySelector(".hamburger-outer"),
-        hbOuterClasses = hbOuter.classList;
+    let html = document.querySelector("html").classList,
+        body = document.querySelector("body").classList,
+        slider = document.querySelector("#js-nav-slider").classList,        hbOuter = document.querySelector(".hamburger-outer"),
+        hamburger = hbOuter.classList;
 
     hbOuter.addEventListener('click', function() {
-        if (hbOuterClasses.contains('active')) {
-            navLinksClasses.remove('slide-in');
-            hbOuterClasses.remove('active');
-            htmlClasses.remove('lock');
-            bodyClasses.remove('lock');
-            navLinksClasses.add('slide-out')
+        if (hamburger.contains('active')) {
+            body.remove('lock');
+            slider.remove('slide-in');
+            hamburger.remove('active');
+            slider.add('slide-out');
             setTimeout(function() {
-                navLinksClasses.add('hidden-mobile');
+                slider.add('hidden-mobile');
             }, 200);
         } else {
-            if (navLinksClasses.contains('hidden-mobile')) {
-                navLinksClasses.remove('hidden-mobile');
-            }
-            navLinksClasses.remove('slide-out');
-            hbOuterClasses.add('active');
-            htmlClasses.add('lock');
-            bodyClasses.add('lock');
-            navLinksClasses.add('slide-in');
+            slider.remove('slide-out');
+            slider.remove('hidden-mobile');
+            hamburger.add('active');
+            body.add('lock');
+            slider.add('slide-in');
         }
     });
 };
